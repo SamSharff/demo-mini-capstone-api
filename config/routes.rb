0683a605f6f28d04b2b4fe67e9944b4b4a4ee889
amanda <amanda@actualize.co>
2022-03-09 14:00:57 -0600

    uses shortcut syntax for route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
- get "/all_products", controller: "products", action: "all_products"
+#  get "/all_products", controller: "products", action: "all_products"
+get "/all_products" => "products#all_products"
 end
