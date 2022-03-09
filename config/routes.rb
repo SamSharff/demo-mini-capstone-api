489654ba12988018953a98a9b551eeadbeb9c9cb
amanda <amanda@actualize.co>
2022-03-09 14:04:20 -0600

    adds first product route and controller action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
 #  get "/all_products", controller: "products", action: "all_products"
 get "/all_products" => "products#all_products"
+get "/first_product" => "products#first_product"
 end
