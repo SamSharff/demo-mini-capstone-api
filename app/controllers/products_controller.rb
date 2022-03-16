61d9e16d8ca00bb2d5fdec1545d131393043eab5
amanda <amanda@actualize.co>
2022-03-16 14:28:51 -0500

    updates products index view template, connected templates, and added sad paths

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,20 +1,20 @@
 class ProductsController < ApplicationController
   def index
-    products = Product.all
-    render json: products.as_json
+    @products = Product.all
+    render template: "products/index"
   end
 
   def create
-    product = Product.new(
+    @product = Product.new(
       name: params[:name],
       price: params[:price],
       image_url: params[:image_url],
       description: params[:description],
     )
-    if product.save
-      render json: product.as_json
+    if @product.save
+      render :show # Same as => `render template: "products/show"`
     else
-      render json: {errors: product.errors.full_messages}, status: 422
+      render json: {errors: @product.errors.full_messages}, status: 422
     end
   end
 
@@ -24,13 +24,16 @@ class ProductsController < ApplicationController
   end
 
   def update
-    product = Product.find_by(id: params[:id])
-    product.name = params[:name] || product.name
-    product.price = params[:price] || product.price
-    product.image_url = params[:image_url] || product.image_url
-    product.description = params[:description] || product.description
-    product.save
-    render json: product.as_json
+    @product = Product.find_by(id: params[:id])
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.image_url = params[:image_url] || @product.image_url
+    @product.description = params[:description] || @product.description
+    if @product.save
+      render :show # Same as => `render template: "products/show"`
+    else
+      render json: {errors: @product.errors.full_messages}, status: 422
+    end
   end
 
   def destroy
