be74d118c1b3c9f93bb776973b9db7dcad392958
amanda <amanda@actualize.co>
2022-03-18 11:57:31 -0500

    uses helper methods for has_many and belongs_to

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,14 +5,16 @@ class Product < ApplicationRecord
   # validates :price, numericality: { greater_than: 0 }
   # validates :description, presence: true
   # validates :description, length: { in: 10..500 }
+  
+  belongs_to :supplier
+  # def supplier
+  #   Supplier.find_by(id: supplier_id)
+  # end
 
-  def supplier
-    Supplier.find_by(id: supplier_id)
-  end
-
-  def images
-    Image.where(product_id: id)
-  end 
+  has_many :images
+  # def images
+  #   Image.where(product_id: id)
+  # end 
 
   def is_discounted?
     price <= 10
