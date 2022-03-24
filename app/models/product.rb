282dba6de857d251c0f107e337c604fea4168a33
Sam Sharff <samanthaleec@gmail.com>
2022-03-24 14:37:34 -0400

    adds categoryproduct model; creates category instances; updates indexr.rb and many other things

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,16 +1,22 @@
 class Product < ApplicationRecord
   has_many :orders
+  belongs_to :supplier
+  has_many :images
+  has_many :category_products
   # validates :name, presence: true
   # validates :name, uniqueness: true
   # validates :price, presence: true
   # validates :price, numericality: { greater_than: 0 }
   # validates :description, presence: true
   # validates :description, length: { in: 10..500 }
-  
-  belongs_to :supplier
-  has_many :images
-   
 
+  has_many :categories, through: :category_products
+  # def categories
+  #   category_products.map do |category_product|
+  #     category_product.category
+  #   end
+  # end
+  
   def is_discounted?
     price <= 10
   end
