c7a93698e30cdca6948c44d8254d50ea132d7237
Sam Sharff <samanthaleec@gmail.com>
2022-03-24 13:07:16 -0400

    removes comments

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,14 +8,8 @@ class Product < ApplicationRecord
   # validates :description, length: { in: 10..500 }
   
   belongs_to :supplier
-  # def supplier
-  #   Supplier.find_by(id: supplier_id)
-  # end
-
   has_many :images
-  # def images
-  #   Image.where(product_id: id)
-  # end 
+   
 
   def is_discounted?
     price <= 10
