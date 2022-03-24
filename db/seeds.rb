3a767c6672c67ac0b22fcc4e62d4458d4d23cf5c
Sam Sharff <samanthaleec@gmail.com>
2022-03-24 13:30:06 -0400

    adds Category model and CategoryProduct model; migrates

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
 
-Supplier.create!(name: "Amazon", email: "amazon@email.com", phone_number: "243222342")
-Supplier.create!(name: "Think Geek", email: "thinkgeek@email.com", phone_number: "5432534")
+# Supplier.create!(name: "Amazon", email: "amazon@email.com", phone_number: "243222342")
+# Supplier.create!(name: "Think Geek", email: "thinkgeek@email.com", phone_number: "5432534")
 
-Product.create!({supplier_id: 2, name: "WNYX Mug", price: "2.99", description: "Get your morning news once you wake up with a cup of joe from... well Joe. He made it with his homemade duct tape"})
-Product.create!({supplier_id: 2, name: "Hitchhiker's Guide to the Galaxy", price: "42.0", description: "In many of the more relaxed civilizations on the Outer Eastern Rim of the Galaxy, the Hitch-Hiker's Guide has already supplanted the great Encyclopaedia Galactica as the standard repository of all knowledge and wisdom, for though it has many omissions and contains much that is apocryphal, or at least wildly inaccurate, it scores over the older, more pedestrian work in two important respects. First, it is slightly cheaper; and secondly it has the words DON'T PANIC inscribed in large friendly letters on its cover."})
-Product.create!({supplier_id: 2, name: "Lightsaber", price: "270.10", description: "Part laser, part samurai sword, all awesome. The lightsaber is an elegant weapon for a more civilized age, not nearly as clumsy as a blaster"})
-Product.create!({supplier_id: 1, name: "Space Cowboy Laser Gun", price: "170.0", description: "This weapon has no other description than, Shiney!"})
-Product.create!({supplier_id: 2, name: "DnD Dice set", price: "57.50", description: "Take down mighty dragons with this timeless set of 20 sided wonders"})
-Product.create!({supplier_id: 1, name: "Sonic Screwdriver", price: "9.99", description: "The Doctor's sciencey magic wand to get out of tight spots. Note: does not work on wood"})
-Product.create!({supplier_id: 2, name: "Yoda sleeping bag", price: "40.00", description: "For real"})
+# Product.create!({supplier_id: 2, name: "WNYX Mug", price: "2.99", description: "Get your morning news once you wake up with a cup of joe from... well Joe. He made it with his homemade duct tape"})
+# Product.create!({supplier_id: 2, name: "Hitchhiker's Guide to the Galaxy", price: "42.0", description: "In many of the more relaxed civilizations on the Outer Eastern Rim of the Galaxy, the Hitch-Hiker's Guide has already supplanted the great Encyclopaedia Galactica as the standard repository of all knowledge and wisdom, for though it has many omissions and contains much that is apocryphal, or at least wildly inaccurate, it scores over the older, more pedestrian work in two important respects. First, it is slightly cheaper; and secondly it has the words DON'T PANIC inscribed in large friendly letters on its cover."})
+# Product.create!({supplier_id: 2, name: "Lightsaber", price: "270.10", description: "Part laser, part samurai sword, all awesome. The lightsaber is an elegant weapon for a more civilized age, not nearly as clumsy as a blaster"})
+# Product.create!({supplier_id: 1, name: "Space Cowboy Laser Gun", price: "170.0", description: "This weapon has no other description than, Shiney!"})
+# Product.create!({supplier_id: 2, name: "DnD Dice set", price: "57.50", description: "Take down mighty dragons with this timeless set of 20 sided wonders"})
+# Product.create!({supplier_id: 1, name: "Sonic Screwdriver", price: "9.99", description: "The Doctor's sciencey magic wand to get out of tight spots. Note: does not work on wood"})
+# Product.create!({supplier_id: 2, name: "Yoda sleeping bag", price: "40.00", description: "For real"})
 
 
