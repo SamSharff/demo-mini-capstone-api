6eb2cf1537cd65c4f5288ea0a531152e3d447a20
Sam Sharff <samanthaleec@gmail.com>
2022-03-28 13:56:31 -0400

    adds in carted_product create action and instance of carted product

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,19 +1,19 @@
 class CartedProductsController < ApplicationController
-  # before_action :authenticate_user
   
   def create
-    carted_product = Carted_product.new(
+    carted_product = CartedProduct.new(
       user_id: current_user.id,
       product_id: params[:product_id],
       quantity: params[:quantity],
-      status: params[:status],
-      order_id: params[:order_id]
+      status: "carted"
+      # order_id: params[:order_id]
     )
-    if carted_product.save
-      render json: { message: "Item, successfully added to cart." }, status: :created
-    else
-      render json: { errors: user.errors.full_messages }, status: :bad_request
-    end
+    carted_product.save
+    render json: carted_product
+    #   render json: { message: "Item, successfully added to cart." }, status: :created
+    # else
+    #   render json: { errors: user.errors.full_messages }, status: :bad_request
+    # end
   end
 
 end
