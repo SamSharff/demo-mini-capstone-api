8832cf2329b7cc516399c7fca0efd8c083bf009b
Sam Sharff <samanthaleec@gmail.com>
2022-03-29 09:53:16 -0400

    adds show.json.jb template view to carted_products; adds instance variable updates to index action in carted_products controller

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,15 +1,15 @@
 class CartedProductsController < ApplicationController
   
   def create
-    carted_product = CartedProduct.new(
+    @carted_product = CartedProduct.new(
       user_id: current_user.id,
       product_id: params[:product_id],
       quantity: params[:quantity],
       status: "carted",
       order_id: params[:order_id]
     )
-    carted_product.save
-    render json: carted_product
+    @carted_product.save
+    render template: "carted_products/show"
   end
 
   def index
